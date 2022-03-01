774ee283641a1e2edfb03dc2beeb5eda1786cdf6
Farhan <animationfar@gmail.com>
2022-03-01 23:26:56 +0700

    [refactor] scene1: refactor scene1 as hooks.

--- js/scene1.js
+++ js/scene1.js
@@ -1,12 +1,12 @@
-function scene1(){
-    var stage = new createjs.Stage("canvas");
+// function scene1(){
+//     var stage = new createjs.Stage("canvas");
 
-    var bg = new createjs.Bitmap("/assets/scene1/background.png");
-    bg.name = "bg";
-    stage.addChild(bg);
+//     var bg = new createjs.Bitmap("/assets/scene1/background.png");
+//     bg.name = "bg";
+//     stage.addChild(bg);
 
-    createjs.Ticker.addEventListener("tick", scene1__update);
-}
+//     createjs.Ticker.addEventListener("tick", scene1__update);
+// }
 
 function scene1__update(){
     updateResolution();
@@ -22,49 +22,79 @@ function scene1__destroy({stage}){
     //
 }
 
-class Scene1 extends createjs.Stage{
-    self = null;
-    _tick = null;
+function scene1(){
+    var stage = new createjs.Stage("canvas");
 
-    stage = null;
+    var bg = new createjs.Bitmap("/assets/scene1/background.png");
+    bg.name = "bg";
+    stage.addChild(bg);
 
-    constructor(props){
-        super(props);
-        this.self = this;
-        this.stage = this;
+    createjs.Ticker.addEventListener("tick", update);
 
-        var {self, stage} = this;
+    function update(){
+        updateResolution(stage);
 
-        var bg = new createjs.Bitmap("/assets/scene1/background.png");
-        bg.name = "bg";
-        stage.addChild(bg);
+        var bg = stage.getChildByName('bg');
+        bg.scale = stage.canvas.clientHeight / 640;
+        bg.x = (stage.canvas.width / 2) - (bg.scale * 360 / 2);
 
-        createjs.Ticker.addEventListener("tick", this.updateScene(self));
+        stage.update();
     }
 
-    /**
-     * Shorthand caller of Scene
-     * @param {HTMLCanvasElement|String|Object} sel css selector syntax of element
-     * @returns createjs.Stage
-     */
-    static init(sel){
-        return new Scene1(sel ?? 'canvas');
-    }
-
-    updateScene({stage}){
-        this._tick = function(e){
-            updateResolution(stage);
-
-            var bg = stage.getChildByName('bg');
-            bg.scale = stage.canvas.clientHeight / 640;
-            bg.x = (stage.canvas.width / 2) - (bg.scale * 360 / 2);
-
-            stage.update();
-        }
-        return this._tick;
-    }
-
-    destroy(){
+    function stop(){
         //
     }
+
+    return {
+        stage,
+        update,
+        stop
+    };
 }
+
+// class Scene1 extends createjs.Stage{
+//     self = null;
+//     _tick = null;
+
+//     stage = null;
+
+//     constructor(props){
+//         super(props);
+//         this.self = this;
+//         this.stage = this;
+
+//         var {self, stage} = this;
+
+//         var bg = new createjs.Bitmap("/assets/scene1/background.png");
+//         bg.name = "bg";
+//         stage.addChild(bg);
+
+//         createjs.Ticker.addEventListener("tick", this.updateScene(self));
+//     }
+
+//     /**
+//      * Shorthand caller of Scene
+//      * @param {HTMLCanvasElement|String|Object} sel css selector syntax of element
+//      * @returns createjs.Stage
+//      */
+//     static init(sel){
+//         return new Scene1(sel ?? 'canvas');
+//     }
+
+//     updateScene({stage}){
+//         this._tick = function(e){
+//             updateResolution(stage);
+
+//             var bg = stage.getChildByName('bg');
+//             bg.scale = stage.canvas.clientHeight / 640;
+//             bg.x = (stage.canvas.width / 2) - (bg.scale * 360 / 2);
+
+//             stage.update();
+//         }
+//         return this._tick;
+//     }
+
+//     destroy(){
+//         //
+//     }
+// }
